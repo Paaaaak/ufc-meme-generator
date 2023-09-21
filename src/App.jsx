8f419d32c8431868e0e34e42df93ebaa66568e16
JaehyeonPaak <wogus392@gmail.com>
2023-09-21 18:29:13 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <div>Select Athlete, Select image</div>
+      <div>Image</div>
+      <div>Original text</div>
+      <div>New text, font type, font size, font weight, font color</div>
+      <div>Download</div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Created by JaehyeonPaak</div>
+      <div>Issue | bug report</div>
+      <div>Github link</div>
+    </footer>
+  );
+};
+
+export default Footer;
