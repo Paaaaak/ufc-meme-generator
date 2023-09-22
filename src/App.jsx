45491afb3c0c7ef4595b358e1f36590ccffd7517
JaehyeonPaak <wogus392@gmail.com>
2023-09-22 17:24:53 +0900

    feat: add frame

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,37 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <div>Select Athlete, Select image</div>
-      <div>Image</div>
-      <div>Original text</div>
-      <div>New text, font type, font size, font weight, font color</div>
-      <div>Download</div>
+      <div className='el-container'>
+        <div>
+          <select>
+            <option value="" disabled selected hidden>Select an athele</option>
+            <option value="conor">Conor McGregor</option>
+            <option value="alex">Alex Pereira</option>
+            <option value="jones">John Jones</option>
+          </select>
+          <select>
+            <option value="" disabled selected hidden>Select an image</option>
+            <option value="option1">옵션 1</option>
+            <option value="option2">옵션 2</option>
+            <option value="option3">옵션 3</option>
+          </select>
+        </div>
+        <div className='el-image-container'>Image</div>
+        <div className='el-condition-container'>
+          <div>
+            <input readOnly type='text' placeholder='Original text'></input>
+          </div>
+          <div>
+            <input type='text' placeholder='New text'></input>
+            <button>Add</button>
+          </div>
+          <div>font type</div>
+          <div>font size</div>
+          <div>font weight</div>
+          <div>font color</div>
+        </div>
+        <div>Download</div>
+      </div>
       <Footer></Footer>
     </div>
   );
